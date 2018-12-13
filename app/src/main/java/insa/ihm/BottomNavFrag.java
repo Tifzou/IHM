60d50499ab2b8c2ad9eb2303b4f893a96401e83e
Tifenn Floch <floch.tifenn@yahoo.fr>
2018-12-13 22:23:32 +0100

    bottom nav bar working for dashboard

--- app/src/main/java/insa/ihm/BottomNavFrag.java
+++ app/src/main/java/insa/ihm/BottomNavFrag.java
@@ -1,95 +1,76 @@
 package insa.ihm;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BottomNavFrag.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BottomNavFrag#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BottomNavFrag extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-
-    private OnFragmentInteractionListener mListener;
-
-    public BottomNavFrag() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment BottomNavFrag.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BottomNavFrag newInstance() {
-        BottomNavFrag fragment = new BottomNavFrag();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bottom_nav, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_bottom_nav, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        /* TODO: do the same thing for Chat button
+        ImageButton mChatButton = (ImageButton) getView().findViewById(R.id.chat);
+        mChatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.group_info:
+                        Intent chatActivity = new Intent(getActivity(), Chat.class);
+                        startActivity(chatActivity);
+                        break;
+                }
+            }
+        });
+         */
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        ImageButton mDashboardButton = view.findViewById(R.id.dashboardButton);
+        mDashboardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.dashboardButton:
+                        Intent dashboardActivity = new Intent(getActivity(), Dashboard.class);
+                        startActivity(dashboardActivity);
+                        break;
+                }
+            }
+        });
+        /* TODO: do the same thing for Tasks button
+        ImageButton mTasksButton = (ImageButton) getView().findViewById(R.id.tasksButton);
+        mTasksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.tasksButton:
+                        Intent tasksActivity = new Intent(getActivity(), Tasks.class);
+                        startActivity(tasksActivity);
+                        break;
+                }
+            }
+        });
+         */
+        /* TODO: do the same thing for Group info button
+        ImageButton mGroupInfoButton = (ImageButton) getView().findViewById(R.id.group_infoButton);
+        mGroupInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.group_infoButton:
+                        Intent groupInfoActivity = new Intent(getActivity(), GroupInfo.class);
+                        startActivity(groupInfoActivity);
+                        break;
+                }
+            }
+        });
+         */
+        return view;
     }
 }
--- app/src/main/java/insa/ihm/Dashboard.java
+++ app/src/main/java/insa/ihm/Dashboard.java
@@ -11,25 +11,7 @@ public class Dashboard extends AppCompatActivity {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +19,6 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
