8eab2b792af807d42d496f8978bae2ceac52cd4b
Pomeg <44205023+Pomeg@users.noreply.github.com>
2018-12-14 09:55:45 +0100

    group activity

--- app/src/main/java/insa/ihm/GroupInfo.java
+++ app/src/main/java/insa/ihm/GroupInfo.java
@@ -2,15 +2,17 @@ package insa.ihm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
-public class MemberListActivity extends Activity {
+public class GroupInfo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_list);
 
         String[] noms = new String[] {"A", "B", "C"};
 
--- app/src/main/java/insa/ihm/MemberListAdapter.java
+++ app/src/main/java/insa/ihm/MemberListAdapter.java
@@ -1,7 +1,9 @@
-package com.example.myapplication;
+package insa.ihm;
 
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,26 +11,27 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MemberListAdapter extends ArrayAdapter<String>{
+public class MemberListAdapter extends ArrayAdapter<String> {
 
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        super.getView(position, convertView, parent);
+//        super.getView(position, convertView, parent);
 
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
 
         View rowView = inflater.inflate(R.layout.member_layout, parent, false);
 
+
         TextView textView = (TextView) rowView.findViewById(R.id.name);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
 
         textView.setText(getItem(position));
 
-        if(convertView == null)
+        if(convertView == null) {
             imageView.setImageResource(R.drawable.contact);
-        else
+        }else
             rowView = convertView;
 
         return rowView;
