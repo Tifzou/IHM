3db2c31e07c4e57642d3c22d56d033ee469fb4b6
firemat <mas.mat@hotmail.fr>
2019-01-06 18:49:46 +0100

    Remove old version of dashboard view for a project and add accordeon like list

--- app/src/main/java/insa/ihm/Dashboard.java
+++ app/src/main/java/insa/ihm/Dashboard.java
@@ -8,31 +8,37 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Dashboard extends AppCompatActivity {
 
     private TextView mTextMessage;
     TextView txt_help_gest;
 
-
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+    List<String> expandableListTitle;
+    HashMap<String, List<String>> expandableListDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        txt_help_gest = (TextView) findViewById(R.id.droptext);
-        // hide until its title is clicked
-        txt_help_gest.setVisibility(View.GONE);
+        expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+        expandableListDetail = Dashboard_ExpandableListDataPump.getData();
+        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
+        expandableListAdapter = new Dashboard_CustomExpandableListAdapter(this, expandableListTitle, expandableListDetail);
+        expandableListView.setAdapter(expandableListAdapter);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-    }
-
-    /**
-     * onClick handler
-     */
-    public void toggle_contents(View v){
-        txt_help_gest.setVisibility( txt_help_gest.isShown()
-                ? View.GONE
-                : View.VISIBLE );
     }
 }
